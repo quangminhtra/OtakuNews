df3279c7bc8a412e376aa1e23ccc5041d56dbd2b
Quang Minh <qminh30k3@gmail.com>
2024-05-05 00:23:31 -0400

    minh

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,121 +1,75 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled from 'styled-components'
 import { useGlobalContext } from '../context/global';
 
-function Sidebar() {
-    const { popularAnime } = useGlobalContext();
 
-    const sorted = popularAnime?.sort((a, b) => b.score - a.score);
+function Sidebar() {
+    const {popularAnime} = useGlobalContext()
+
+    const sorted = popularAnime?.sort((a,b) => {
+        return b.score - a.score
+    })
 
     return (
         <SidebarStyled>
             <h3>Top 5 Popular</h3>
             <div className="anime">
-                {sorted?.slice(0, 5).map((anime) => (
-                    <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                {sorted?.slice(0,5).map((anime) => {
+                    return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
                         <img src={anime.images.jpg.large_image_url} alt="" />
-                        <h5>{anime.title}</h5>
+                        <h5>
+                            {anime.title}
+                        </h5>
                     </Link>
-                ))}
+                })}
             </div>
         </SidebarStyled>
-    );
+    )
 }
 
 const SidebarStyled = styled.div`
     margin-top: 2rem;
     background-color: rgb(35, 45, 63);
     border-top: 5px solid #e5e7eb;
-    padding: 2rem 1rem 2rem 5rem;
-
+    padding-right: 1rem;
+    padding-left: 5rem;
+    padding-top: 2rem;
     h3 {
-        background: linear-gradient(to right, #A855F7 10%, #4FFFB0);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        background-clip: text;
-        text-fill-color: transparent;
-        font-size: 2rem;
-        margin-bottom: 1rem;
+        
+            
+    background: linear-gradient(to right, #A855F7 10%, #4FFFB0);
+    -webkit-background-clip: text;  /* Clip the background to the text */
+    -webkit-text-fill-color: transparent;  /* Make the text fill transparent */
+     background-clip: text;  /* Clip the background to the text for non-WebKit browsers */
+    text-fill-color: transparent;  /* Transparent fill for non-WebKit browsers */
+    font-size: 35px;
+    padding-bottom: 1rem;
+    padding-right: 20px
+    padding-left: 20px
     }
-
-    .anime {
+    .anime{
         display: flex;
         flex-direction: column;
-        width: 200px;
-
-        img {
+        width: 150px;
+        img{
             width: 100%;
             border-radius: 10px;
             border: 5px solid rgb(250, 249, 246);
         }
-
-        a {
+        a{
             margin-top: 2rem;
             display: flex;
             flex-direction: column;
-            gap: 0.2rem;
+            gap: .2rem;
             color: rgb(65, 176, 110);
-
-            h5 {
+            h5{
                 font-size: 1.1rem;
                 align-self: center;
-                text-align: center;
             }
-        }
-    }
-
-    @media (max-width: 1200px) {
-        .anime {
-            width: 150px;
-
-            img {
-                border: 4px solid rgb(250, 249, 246);
-            }
-
-            a h5 {
-                font-size: 1rem;
-            }
-        }
-    }
-
-    @media (max-width: 768px) {
-        padding: 2rem 1rem;
-
-        h3 {
-            font-size: 1.75rem;
-        }
-
-        .anime {
-            width: 120px;
-
-            img {
-                border: 3px solid rgb(250, 249, 246);
-            }
-
-            a h5 {
-                font-size: 0.9rem;
-            }
-        }
-    }
-
-    @media (max-width: 576px) {
-        h3 {
-            font-size: 1.5rem;
-        }
-
-        .anime {
-            width: 100px;
-
-            img {
-                border: 2px solid rgb(250, 249, 246);
-            }
-
-            a h5 {
-                font-size: 0.8rem;
-            }
+            
         }
     }
 `;
 
-export default Sidebar;
+export default Sidebar
